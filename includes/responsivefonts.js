8c07e57eb4d38945ac312520de383c6e22c83979
unknown <ckt.lucas@CKTPMDV09.redeconekta.local>
2015-10-26 15:07:08 -0200

    ..

---
+++ includes/responsivefonts.js
@@ -0,0 +1,15 @@
+$('#desc1').flowtype({
+   minimum   : 320,
+   maximum   : 1200,
+   minFont   : 14,
+   maxFont   : 18,
+   fontRatio : 30
+});
+
+$('#titulo2').flowtype({
+   minimum   : 320,
+   maximum   : 1200,
+   minFont   : 35,
+   maxFont   : 60,
+   fontRatio : 25
+});
